3e462d155ca04bcbd3fe9600682db75cbe205deb
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-02 19:07:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('start')
+
+var counterYes = 0;
+var counterNo = 0;
+
+// questions
+
+var question ("How many tacos can I eat?")
+var question ("")
+var question ("")
+
+
+
+console.log('end')
