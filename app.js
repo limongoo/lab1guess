a46c42aec339e2ddebfac9e48d10dfec2e33f7ef
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-03 19:12:05 -0700

    added image, and clean up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('start');
 
-var question1 = prompt("Do I like tacos?", "yes/no");
+// question 1
+var question1 = prompt("Do I like tacos?", "yes or no");
   if (question1 === "yes") {
     alert ("You are correct, I love tacos!");
   }
@@ -8,25 +9,28 @@ var question1 = prompt("Do I like tacos?", "yes/no");
     alert ("Sadface, you guessed incorrectly.");
   }
 
+// question 2
 var correct = "Indonesia";
-var question2 = prompt("Where was I born? (Indonesia, Japan or Thailand");
+var question2 = prompt("Where was I born? Indonesia, Japan or Thailand");
   if (question2 === correct.toLowerCase()) {
     alert ("You are correct!");
   }
   else {
     alert ("Try again please.");
     while (question2 != correct.toLowerCase()) {
-      prompt("Where was I born? (Indonesia, Japan or Thailand");
+      prompt("Where was I born? Indonesia, Japan or Thailand");
       if (correct.toLowerCase()){
         alert ("You are correct!");
-        break;
+
       }
       else {
         alert ("Try again please.");
       }
+      break;
     }
   }
 
+// question 3
 var question3 = prompt("What's my favorite color?");
   if (question3 === "black") {
     alert ("You are correct!");
