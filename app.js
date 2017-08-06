7edb74e5fd0f725b1f1a57d5e0b2a05e1055776b
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-06 09:55:42 -0700

    add counter

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log('start');
 
+var counterCorrect = 0;
+var counterWrong = 0;
+
 // quiz function
 function promptQuiz(){
 
@@ -7,9 +10,11 @@ function promptQuiz(){
 var question1 = prompt("Do I like tacos?", "yes or no");
   if (question1 === "yes") {
     alert ("You are correct, I love tacos!");
+    counterCorrect++;
   }
   else {
     alert ("Sadface, you guessed incorrectly.");
+    counterWrong++;
   }
 
 // question 2
@@ -19,10 +24,12 @@ var question2 = "";
       question2 = prompt("Where was I born? Indonesia, Japan or Thailand");
       if (question2 === correct.toLowerCase()){
         alert ("You are correct!");
+        counterCorrect++;
         break;
       }
       else {
         alert ("Try again please.");
+        counterWrong++;
       }
     }
 
@@ -30,12 +37,18 @@ var question2 = "";
 var question3 = prompt("What's my favorite color?");
   if (question3 === "black") {
     alert ("You are correct!");
+    counterCorrect++;
   }
   else {
     alert ("You are wrong.");
+    counterWrong++;
   }
-
 }
 
+// Report counter
+document.getElementById('yes').innerHTML = counterCorrect;
+document.getElementById('no').innerHTML = counterWrong;
 
+console.log(counterCorrect);
+console.log(counterWrong);
 console.log('end');
