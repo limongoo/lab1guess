a79baebd016bf6b6ab973c6562f849fb1b9fc951
Ivan Limongan <ivan.limongan@gmail.com>
2017-08-08 19:27:42 -0700

    code cleanup

--- app.js
+++ app.js
@@ -43,50 +43,6 @@ function askQuiz(question, answer, result) {
 
 
 
-// question 1
-// var question1 = prompt("Do I like tacos?", "yes or no");
-//   if (question1 === "yes") {
-//     alert ("You are correct, I love tacos!");
-//     counterCorrect+=1;
-//     totalClick++;
-//   }
-//   else {
-//     alert ("Sadface, you guessed incorrectly.");
-//     counterWrong+=1;
-//     totalClick++;
-//   }
-//
-// // question 2
-// var correct = "Indonesia";
-// var question2 = "";
-//     while (question2 != correct.toLowerCase()) {
-//       question2 = prompt("Where was I born? Indonesia, Japan or Thailand");
-//       if (question2 === correct.toLowerCase()){
-//         alert ("You are correct!");
-//         counterCorrect+=1;
-//         totalClick++;
-//         break;
-//       }
-//       else {
-//         alert ("Try again please.");
-//         counterWrong+=1;
-//         totalClick++;
-//       }
-//     }
-//
-// // question 3
-// var question3 = prompt("What's my favorite color?");
-//   if (question3 === "black") {
-//     alert ("You are correct!");
-//     counterCorrect+=1;
-//     totalClick++;
-//   }
-//   else {
-//     alert ("You are wrong.");
-//     counterWrong+=1;
-//     totalClick++;
-//   }
-
 // add counter to page
 document.getElementById('yes').innerHTML = counterCorrect;
 document.getElementById('no').innerHTML = counterWrong;
